62c8d446c6a92232a2eeac91aa2bc4e5722e3108
Dully6512 <gmlqls785@gmail.com>
2021-06-10 10:52:48 +0900

    상품클래스, 아이스크림 클래스 생성
    아이스크림 클래스 상품클래스 참조
    상품클래스 getter,setter 추가

---
+++ Icecreamshopmanagement/src/icecreamshopmanagement/Icecream.java
@@ -0,0 +1,14 @@
+package icecreamshopmanagement;
+
+public class Icecream extends Product{
+	public Icecream(String tempId, String tempName, String tempPrice, String tempFlavor) {
+		
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.flavor = flavor;
+		
+		
+	}
+
+}
---
+++ Icecreamshopmanagement/src/icecreamshopmanagement/Product.java
@@ -0,0 +1,29 @@
+package icecreamshopmanagement;
+
+public class Product {
+	String id;
+	String name;
+	String price;
+	String flavor;
+	
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFlavor() {
+		return flavor;
+	}
+	public void setFlavor(String flavor) {
+		this.flavor = flavor;
+	}
+	
+
+}
