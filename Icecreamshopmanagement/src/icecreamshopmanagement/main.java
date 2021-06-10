ee37263b99a7729b5b80f34334cfae9d9d5df85f
Dully6512 <gmlqls785@gmail.com>
2021-06-10 11:05:55 +0900

    4번 json으로 파일 저장하기 메소드 추가 및 주석 추가

--- Icecreamshopmanagement/src/icecreamshopmanagement/main.java
+++ Icecreamshopmanagement/src/icecreamshopmanagement/main.java
@@ -2,17 +2,40 @@ package icecreamshopmanagement;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
 
 public class main {
+	/*
+	   * @ 메소드명 : SaveJson
+	   * @ breif  : Json파일을 저장하는 기능
+	   * @ detail : jsonObject에 저장된 내용을 선언한 위치에 Json파일로 내보냅니다.
+	   * @ 작성자   : 신희빈
+	   * @ 작성일자 : 2021 - 06 -10
+	   */
+	  
+	  public void SaveJson(JSONArray jsonArray) throws IOException {
+		  JSONObject jObj = new JSONObject();			// json오브젝트 저장하기위해 선언
+			jObj.put("Dress", jsonArray);				// 출력 할때 앞에 "Dress"가 붙도록
+			FileWriter writer = new FileWriter("src/json/mydata.json");	// json파일 저장할 위치
+			writer.write(jObj.toJSONString());	//String 형태로 저장
+			writer.flush();
+			writer.close();
+	  }
+	  
+	  
+		  
+  public static void main(String[] args) throws IOException, org.json.simple.parser.ParseException{
 
-  public static void main(String[] args) throws IOException {
-
-
+	main Main = new main();
   	JSONArray jsonArray = new JSONArray();
     boolean swi = true; 
 		while(swi){
@@ -20,8 +43,8 @@ public class main {
 	System.out.println("1. ");
 	System.out.println("2. ");
 	System.out.println("3. ");
-	System.out.println("4. json으로 정보보기");
-	System.out.println("5. json으로 내보내기");
+	System.out.println("4. json으로 파일 저장하기");
+	System.out.println("5. ");
     System.out.println("7. 나가기");
 			
       
@@ -44,11 +67,11 @@ public class main {
 					break;
 
 				case 4:
-					
+					System.out.println("파일을 내보냅니다.");
+					Main.SaveJson(jsonArray);
 					break;
           
 				case 5:
-					
 					break;
 
 				case 6: 
